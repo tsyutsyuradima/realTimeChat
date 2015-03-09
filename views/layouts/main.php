ebcd6dfac01f5b1306dff6a3d6f0af3aba06921f
AntonAkopyan <anton.akopyan@joyrocks.com>
2015-03-09 23:49:27 +0200

    little chat ))

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -27,7 +27,7 @@ AppAsset::register($this);
     <div class="wrap">
         <?php
             NavBar::begin([
-                'brandLabel' => 'My Company',
+                'brandLabel' => 'Real Time Chat',
                 'brandUrl' => Yii::$app->homeUrl,
                 'options' => [
                     'class' => 'navbar-inverse navbar-fixed-top',
@@ -54,13 +54,6 @@ AppAsset::register($this);
             <?= $content ?>
         </div>
     </div>
-
-    <footer class="footer">
-        <div class="container">
-            <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-            <p class="pull-right"><?= Yii::powered() ?></p>
-        </div>
-    </footer>
 
 <?php $this->endBody() ?>
 </body>
--- views/site/chat.php
+++ views/site/chat.php
@@ -4,50 +4,98 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 $this->title = 'Chat';
 ?>
-
-<script src="/js/js/server/node_modules/socket.io/node_modules/socket.io-client/dist/socket.io.js"></script>
-<script src="/js/js/client/client.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="/web/js/js/server/node_modules/socket.io/node_modules/socket.io-client/dist/socket.io.js"></script>
+<script src="/web/js/js/client/client.js"></script>
 <script>
     var socket = new YiiNodeSocket();
     socket.debug(true);
     var testRoom;
 
+    var room = socket.room('chatRoom').join(function (success, numberOfRoomSubscribers) {
+
+        alert(this.);
+        if (success && numberOfRoomSubscribers <= 2) {
+            if (numberOfRoomSubscribers == 1) {
+                $('#wait').css({ 'display': "block" });
+            }
+            else {
+                $('#chat').css({ 'display': "block" });
+                this.emit('toJoin' , {
+                    message : {
+                        id : <?=Yii::$app->getUser()->identity->id?>,
+                        username : '<?=Yii::$app->getUser()->identity->username?>'
+                    }
+                });
+            }
+        } else {
+            this.leave();
+            $('#excessive').css({ 'display': "block" });
+        }
+    });
+
     socket.onConnect(function () {
-        var room = socket.room('chatRoom').join(function (success, numberOfRoomSubscribers) {
-            if (success && numberOfRoomSubscribers <= 2) {
-                alert(numberOfRoomSubscribers + ' clients in room: ');
-                // do something
-
-                // bind events
-                this.on('join', function (newMembersCount) {
-                    alert(numberOfRoomSubscribers + ' clients in joinjoinjoinjoin: ');
-                });
-
-                this.on('data', function (data) {
-                    alert(numberOfRoomSubscribers + ' clients in datadatadatadata: ');
-                });
-
-                this.on('test', function (data) {
-                    alert(numberOfRoomSubscribers + ' clients in TEST TEST: ');
-                });
-
+        room.on('sendMessage', function (data) {
+            if (data.message.msg === '') return false;
+            var who = '';
+            if (data.message.id == <?=Yii::$app->getUser()->identity->id?>) {
+                who = 'me';
             } else {
-                // numberOfRoomSubscribers - error message
-                alert(numberOfRoomSubscribers);
+                who = 'you';
+            }
+            $('.chatList').append('<li class="' + who + '"><span class="name">' + data.message.username + '</span><span class="msg">' + data.message.msg + '</span></li>');
+        });
+
+        room.on('sendSystemMessage', function (data) {
+            $('.chatList').append('<li class="system">' + data.message.username + ' ' + data.message.msg + '</li>');
+        });
+
+        room.on('toJoin', function (data) {
+            if (data.message.id == <?=Yii::$app->getUser()->identity->id?>) {
+                $('.chatList').append('<li class="system">You have successfully joined!</li>');
+            }
+            else
+            {
+                $('#wait').css({ 'display': "none" });
+                $('#chat').css({ 'display': "block" });
+                $('.chatList').append('<li class="system">' + data.message.username + ' have successfully joined!</li>');
             }
         });
 
         $("#send").on("click", function () {
-            room.emit('test' , {
+            var msg = $("#message textarea");
+            room.emit('sendMessage' , {
                 message : {
-                    id : 12,
-                    title : 'This is a test message'
+                    id : <?=Yii::$app->getUser()->identity->id?>,
+                    username : '<?=Yii::$app->getUser()->identity->username?>',
+                    msg : msg.val()
                 }
             });
+            msg.val('').focus();
+            $('.chatList').animate({ scrollTop: $(".chatList")[0].scrollHeight }, 100);
+
         });
 
+        $('#message textarea').keypress(function(e){
+            if(e.which == 13) {
+                e.preventDefault();
+                $("#send").trigger('click');
+            }
+
+        });
     });
 </script>
-
-<i id="send">SEND MESSSAGE</i>
+<div id="excessive" style="display: none;">
+    Oops...
+</div>
+<div id="wait" style="display: none;">
+    Wait...
+</div>
+<div id="chat" style="display: none;">
+    <ul class="chatList"></ul>
+    <div id="message">
+        <textarea></textarea>
+        <button id="send">Send</button>
+    </div>
+</div>
 
